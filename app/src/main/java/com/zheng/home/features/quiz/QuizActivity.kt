844cc74b2df128e85e7a38f16d7611080fea6519
dz <dz@dzs-MacBook-Pro.local>
2018-04-05 13:42:26 -0700

    clear error

--- app/src/main/java/com/zheng/home/features/quiz/QuizActivity.kt
+++ app/src/main/java/com/zheng/home/features/quiz/QuizActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.zheng.home.R
+import com.zheng.home.R.id.fab
 
 import kotlinx.android.synthetic.main.activity_quiz.*
 
@@ -12,12 +13,6 @@ class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
